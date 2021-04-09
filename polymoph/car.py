113799c0cae1797114f25e1b7a2a789e2e145482
ygberg <fredrik.ygberg@gmail.com>
2021-04-09 16:47:22 +0200

    polymorph labb

--- polymoph/car.py
+++ polymoph/car.py
@@ -4,7 +4,7 @@ class Car(Vec):
     def __init__ (self,fule,consumtion):
         self.fule = fule
         self.consumtion = consumtion + 0.9
-        self.tank = fule
+        
     
 
              
